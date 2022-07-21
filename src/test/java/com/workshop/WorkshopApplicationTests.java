bd9b15053405ae1294e5114721503dc7fb26318d
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-07-21 11:44:57 -0300

    Initial commit

---
+++ src/test/java/com/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
