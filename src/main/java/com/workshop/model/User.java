65f6e6235b6a6b5824d0a6f017e7974169860dab
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-07-22 22:38:04 -0300

    Criando Entidade User e seu EndPoint UserController

---
+++ src/main/java/com/workshop/model/User.java
@@ -0,0 +1,37 @@
+package com.workshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 7189149854250575515L;
+
+    private Long id;
+    private String name;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
