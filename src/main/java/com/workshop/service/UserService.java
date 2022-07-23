24754ade50c52548b54d9d122c11923c97e39b33
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-07-22 22:49:35 -0300

    Implementando classe de serviço do User

---
+++ src/main/java/com/workshop/service/UserService.java
@@ -0,0 +1,10 @@
+package com.workshop.service;
+
+import com.workshop.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+}
---
+++ src/main/java/com/workshop/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.workshop.service.impl;
+
+import com.workshop.model.User;
+import com.workshop.repository.UserRepository;
+import com.workshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
