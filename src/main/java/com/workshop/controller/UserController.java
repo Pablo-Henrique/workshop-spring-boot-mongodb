1ab578be461278ae508eccd241aff63b28255e54
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-07-24 16:23:03 -0300

    Retornando DTO na requisição

--- src/main/java/com/workshop/controller/UserController.java
+++ src/main/java/com/workshop/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.workshop.controller;
 
+import com.workshop.dto.UserDTO;
 import com.workshop.model.User;
 import com.workshop.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,9 +23,10 @@ public class UserController {
     private UserService service;
 
     @GetMapping(path = "/users")
-    public ResponseEntity<List<User>> findAll() {
-        List<User> users = service.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> usersDTO = new ArrayList<>();
+        BeanUtils.copyProperties(service.findAll(), usersDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(usersDTO);
     }
 
 }
