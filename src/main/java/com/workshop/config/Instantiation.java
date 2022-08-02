242fcb9d5b07013a192aa40d20f760951ce7040e
Pablo Henrique <pablohenrique.oficial@hotmail.com>
2022-08-02 11:03:50 -0300

    Data seeding

---
+++ src/main/java/com/workshop/config/Instantiation.java
@@ -0,0 +1,47 @@
+package com.workshop.config;
+
+import com.workshop.dto.AuthorDTO;
+import com.workshop.model.Post;
+import com.workshop.model.User;
+import com.workshop.repository.PostRepository;
+import com.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userReposiroty;
+
+    @Autowired
+    private PostRepository postReposiroty;
+
+    @Override
+    public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userReposiroty.deleteAll();
+        postReposiroty.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        postReposiroty.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userReposiroty.save(maria);
+    }
+}
